487ac195c80b5508eea08c0fa8362c7e485c21cc
Kofi Oduro <kofia.oduro@gmail.com>
2025-11-05 09:52:53 -0500

    added a docker file"

--- project1/db.js
+++ project1/db.js
@@ -1,7 +1,7 @@
 const { Pool } = require('pg');
 const pool = new Pool({
   user: 'postgres',
-  host: 'localhost',
+  host: "host.docker.internal",
   database: 'school',
   password: 'Frootloop5',
   port: 5432,
